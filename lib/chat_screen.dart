9c4fceb47c36ede339968874a87a8ff3351e6b8e
imMatheusMagalhaes <magalhaes.almeida.matheus@gmail.com>
2021-02-08 23:34:11 -0300

    first commit

---
+++ lib/chat_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/text_composer.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _sendMessage({String text, File imgFile}) async {
+    Map<String, dynamic> data = {};
+
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().microsecondsSinceEpoch.toString())
+          .putFile(imgFile);
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data["imgFile"] = url;
+    }
+    if (text != null) data["text"] = text;
+    Firestore.instance.collection("messages").add(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Olá"),
+        elevation: 0,
+      ),
+      body: TextComposer(_sendMessage),
+    );
+  }
+}
